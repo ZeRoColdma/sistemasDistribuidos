14e5c8f2b46c7d7027b1940e6f39ed5b52ea52b3
Carlos Alberto <ccarloos9@gmail.com>
2020-09-20 17:08:52 -0300

    Rotas

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const routes = express.Router();
 
 const ProdutoCotroller = require('./src/controller/ProdutosController');
 
-routes.get('/', ProdutoCotroller.index);
+routes.get('/index', ProdutoCotroller.index);
 routes.post('/novoproduto', ProdutoCotroller.store);
 
 module.exports = routes;
