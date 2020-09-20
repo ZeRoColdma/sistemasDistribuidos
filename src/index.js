769692351907e8a35fc00cef58695fb9130bdf17
Carlos Alberto <ccarloos9@gmail.com>
2020-09-20 17:20:21 -0300

    pasrse

--- src/index.js
+++ src/index.js
@@ -10,16 +10,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(routes);
 
-app.use(function (req, res, next) {
-  req.io = io;
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
-  next();
-});
-
 port = process.env.PORT || 3333;
 
 app.listen(port);
